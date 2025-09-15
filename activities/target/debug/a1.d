7a122534a4757d55436f47d00f92358e34ee957c
Stefanroets180 <stefanroetscode@gmail.com>
2025-09-15 18:41:37 +0200

    first commit

---
+++ activities/target/debug/a1.d
@@ -0,0 +1,1 @@
+/Users/stefanroets/RustroverProjects/Rust\ -\ Complete\ Guide/activities/target/debug/a1: /Users/stefanroets/RustroverProjects/Rust\ -\ Complete\ Guide/activities/src/bin/a1.rs
---
+++ activities/target/debug/deps/a1-8e30292319b75ede.d
@@ -0,0 +1,5 @@
+/Users/stefanroets/RustroverProjects/Rust - Complete Guide/activities/target/debug/deps/a1-8e30292319b75ede.d: src/bin/a1.rs
+
+/Users/stefanroets/RustroverProjects/Rust - Complete Guide/activities/target/debug/deps/liba1-8e30292319b75ede.rmeta: src/bin/a1.rs
+
+src/bin/a1.rs:
