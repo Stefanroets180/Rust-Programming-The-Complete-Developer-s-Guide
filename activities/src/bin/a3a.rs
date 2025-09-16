b72b6d2a92b867690525f6b502baa7d43830a356
Stefanroets180 <stefanroetscode@gmail.com>
2025-09-16 16:00:11 +0200

    15.-Activity-Logic-with-If-_-Else

--- activities/src/bin/a3a.rs
+++ activities/src/bin/a3a.rs
@@ -1,11 +1,20 @@
-// Topic: Basic arithmetic
+// Topic: Flow control using if..else
 //
 // Program requirements:
-// * Displays the result of the sum of two numbers
+// * Displays a message based on the value of a boolean variable
+// * When the variable is set to true, display "hello"
+// * When the variable is set to false, display "goodbye"
 //
 // Notes:
-// * Use a function to add two numbers together
-// * Use a function to display the result
-// * Use the "{:?}" token in the println macro to display the result
+// * Use a variable set to either true or false
+// * Use an if..else block to determine which message to display
+// * Use the println macro to display messages to the terminal
 
-fn main() {}
+fn main() {
+    let my_bool = false;
+    if my_bool == true {
+        println!("hello");
+    } else {
+        println!("goodbye");
+    }
+}
