01a5ece8f1cbf0978d46ca93a8c9e5143c1bddd1
Stefanroets180 <stefanroetscode@gmail.com>
2025-09-16 11:00:58 +0200

    3d commit

--- activities/src/bin/a2.rs
+++ activities/src/bin/a2.rs
@@ -8,6 +8,7 @@
 // * Use a function to display the result
 // * Use the "{:?}" token in the println macro to display the result
 
+/*
 fn sub(a: i32, b: i32) -> i32 {
     a - b
 }
@@ -29,4 +30,22 @@ fn main() {
     println!("Rem: {}", rem);
     let rem2 = 6 % 4;
     println!("Rem2: {}", rem2);
+}
+*/
+
+
+// * Use a function to add two numbers together
+fn sum(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+// * Use a function to display the result
+fn display_result(result: i32) {
+    // * Use the "{:?}" token in the println macro to display the result
+    println!("{:?}", result);
+}
+
+fn main() {
+    let result = sum(2, 2);
+    display_result(result);
 }
