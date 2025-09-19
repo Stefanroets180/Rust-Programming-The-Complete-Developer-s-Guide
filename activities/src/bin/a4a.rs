a30a1dbc8e7c2394279fc06faea71f97c29520f2
Stefanroets180 <stefanroetscode@gmail.com>
2025-09-19 15:02:21 +0200

    17-21 Decision making with match

---
+++ activities/src/bin/a4a.rs
@@ -0,0 +1,20 @@
+// Topic: Decision making with match
+//
+// Program requirements:
+// * Display "one", "two", "three", or "other" based on whether
+//   the value of a variable is 1, 2, 3, or some other number,
+//   respectively
+//
+// Notes:
+// * Use a variable set to any integer
+// * Use a match expression to determine which message to display
+// * Use an underscore (_) to match on any value
+
+fn main() {
+    let my_bool = false;
+
+    match my_bool {
+        true => println!("it's true"),
+        false => println!("it's false"),
+    }
+}
--- activities/src/bin/demo3.rs
+++ activities/src/bin/demo3.rs
@@ -1,3 +1,10 @@
 fn main() {
+    let my_name = "Jayson";
+    match my_name{
+        "Jayson" => println!("that is my name"),
+        "Bob" => println!("that not name"),
+        "Alice" => println!("hello alice"),
+        _ => println!("nice to meet you!"),
+    }
 
 }
